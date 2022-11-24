b3c2d94a6ca9d8aa97368f0acc308cb6456ac496
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2022-11-24 17:15:18 -0300

    feat: Removing IsNotEmpty for Dev Development

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,44 +3,36 @@ import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator'
 export class CreateUserDto {
   @MaxLength(100)
   @IsString()
-  @IsNotEmpty()
   name: string
 
   @MaxLength(150)
   @IsString()
-  @IsNotEmpty()
   lastName: string
 
   @MaxLength(150)
   @IsString()
-  @IsNotEmpty()
   username: string
 
   @MaxLength(100)
   @IsString()
-  @IsNotEmpty()
   email: string
 
   @MaxLength(30)
   @IsString()
-  @IsNotEmpty()
   password: string
 
   @MaxLength(30)
   @IsString()
-  @IsNotEmpty()
   phone: string
 
   @MaxLength(14)
   @IsString()
-  @IsNotEmpty()
   cpf: string
 
   @MaxLength(255)
   @IsString()
-  @IsNotEmpty()
   biography: string
 
-  // @IsString()
-  // profileImage: string
+  @IsString()
+  profileImage: string
 }
